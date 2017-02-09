0e76419b6e3fb5923b0d6be980738dd762e7bdc0
Kallirroi <kallirroi.retzepi@gmail.com>
2017-02-09 16:04:19 -0500

    added values to OBI charts, added reset button to time selector

--- scripts/grid_map.js
+++ scripts/grid_map.js
@@ -936,12 +936,20 @@ function charts(data, selectedCharts) {
         // .on('renderlet', function(_chart){
         //   _chart.selectAll("rect.bar").on("click", _chart.onClick);
         // })
+        .on('renderlet', function(chart){
+            var mean = d3.mean(window.newData, function(el){return el.OBIpercentage>0;});
+            bindSmallText(Math.round(mean*100/24), "#OBIpercent_digits");
+        })
         .gap(10)
-        // .yAxisLabel("Cells", 10)
         .on('postRender', function(chart) {
             chart.svg().append('text').attr('class', 'y-label').attr('text-anchor', 'middle')
                 .attr('x', -60).attr('y', 35).attr('dy', '-25').attr('transform', 'rotate(-90)')
                 .text('# OF CELLS').style("fill", "white").style("font-family", "Dosis").style("font-weight", "300")
+                .style("font-size", "8px");
+
+            chart.svg().append('text').attr('class', 'x-label').attr('text-anchor', 'middle')
+                .attr('x', 170).attr('y', 120).attr('dy', '-25')
+                .text('PERCENTAGE OF OPEN BUSINESSES').style("fill", "white").style("font-family", "Dosis").style("font-weight", "300")
                 .style("font-size", "8px")
         }) 
         .yAxis().ticks(2);
@@ -957,14 +965,23 @@ function charts(data, selectedCharts) {
         .x(d3.scale.linear().domain([0, 150]))
         .y(d3.scale.linear().domain([0, 800]))        
         .centerBar(true)
-        // .elasticY(true)
+        .elasticY(true)
         .gap(1)
         .brushOn(true)
+        .on('renderlet', function(chart){
+            var mean = d3.mean(window.newData, function(el){return el.OBIaverage>0;});
+            bindSmallText(Math.round(mean/24), "#OBIaverage_digits");
+        })
         .on('postRender', function(chart) {
             chart.svg().append('text').attr('class', 'y-label').attr('text-anchor', 'middle')
                 .attr('x', -60).attr('y', 35).attr('dy', '-25').attr('transform', 'rotate(-90)')
-                .text('# OF BUSINESSES').style("fill", "white").style("font-family", "Dosis").style("font-weight", "300")
-                .style("font-size", "8px")
+                .text('# OF CELLS').style("fill", "white").style("font-family", "Dosis").style("font-weight", "300")
+                .style("font-size", "8px");
+
+            chart.svg().append('text').attr('class', 'x-label').attr('text-anchor', 'middle')
+                .attr('x', 170).attr('y', 120).attr('dy', '-25')
+                .text('AVERAGE # OF OPEN BUSINESSES').style("fill", "white").style("font-family", "Dosis").style("font-weight", "300")
+                .style("font-size", "8px");
         }) 
         .yAxis().ticks(2)
 
@@ -1428,8 +1445,8 @@ function timeSelector(chartWidth,chartHeight) {
     var end = 24; //ending point of brush on chart
     var start0 = 0; //starting point for code before anyone interacts with brush
     var end0 = 24; //ending point for code before anyone interacts with brush
-    var margin = { top: 10, left: 20, right: 0, bottom: 0 },
-        width = chartWidth - margin.right,
+    var margin = { top: 10, left: 10, right: 0, bottom: 0 },
+        width = 280,
         height = 32;
 
     var svg = d3.select("#time_selector").append("svg")
@@ -1440,7 +1457,7 @@ function timeSelector(chartWidth,chartHeight) {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 	svg.append("rect")
-	    .attr("width", 280)
+	    .attr("width", width-30)
 	    .attr("height", 2)
         .attr("fill", "rgb(50,50,50)").attr("stroke","rgba(255,255,255,0.3)");
 
@@ -1449,7 +1466,7 @@ function timeSelector(chartWidth,chartHeight) {
 
 	var x = d3.scale.linear()
         .domain([start0,end0])
-		.range([0, 280]);
+		.range([0, width-30]);
 
 	var brush = d3.svg.brush()
 		.x(x).extent([start,end])
@@ -1539,6 +1556,9 @@ function filterhour(data, rdstart, rdend){
 
 }
 
+function timeSelectorReset() {
+    console.log('should reset')
+};
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
 //  updateOBI(dataUpdate, start, end)  --- dc.js ---                          //
@@ -1575,7 +1595,13 @@ function updateOBI(dataUpdate,start,end){
     .width(chartWidth_)
     .height(chartHeight_)
     .group(OBIpercentGroup_)
-    .dimension(OBIpercentDimension_);   
+    .dimension(OBIpercentDimension_)
+    .elasticY(true)
+    .on('renderlet', function(chart){
+        var mean = d3.mean(dataUpdate, function(el){return el.OBIpercentage>0;});
+        bindSmallText(Math.round(mean/(end-start)), "#OBIpercent_digits");
+    })
+
 
 
     var OBIaverageDimension_ = cf.dimension(function (d) { return d.OBIaverage });
@@ -1584,7 +1610,12 @@ function updateOBI(dataUpdate,start,end){
     .width(chartWidth_)
     .height(chartHeight_)
     .group(OBIaverageGroup_)
-    .dimension(OBIaverageDimension_);    
+    .dimension(OBIaverageDimension_)
+    .elasticY(true)
+    .on('renderlet', function(chart){
+        var mean = d3.mean(dataUpdate, function(el){return el.OBIaverage>0;});
+        bindSmallText(Math.round(mean/(end-start)), "#OBIaverage_digits");
+    })
 
 
     dc.redrawAll();
